641c10151390c4bf9d7c0d5371e405baf0a084fc
Joel <jchinop@unsa.edu.pe>
2026-03-18 18:48:23 -0500

    base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
 
+  build: {
+    outDir: 'bck/internal/ui/dist',
+    emptyOutDir: true,
+  },
+
   resolve: {
     alias: {
       '@/*': '/src/*',
